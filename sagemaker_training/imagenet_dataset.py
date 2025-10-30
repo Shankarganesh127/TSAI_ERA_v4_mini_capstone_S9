1658a30980dabfb8224c806f1c7b2d8e8e8c8bfa
Shankarganesh Kumaraganesan <shankar7ganesh.uk@gmail.com>
2025-10-30 12:37:19 +0000

    transform doubled issue fixed

--- sagemaker_training/imagenet_dataset.py
+++ sagemaker_training/imagenet_dataset.py
@@ -42,10 +42,10 @@ def make_train_transform(img_size: int = 224):
             transforms.RandomResizedCrop(img_size),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(
-                mean=(0.485, 0.456, 0.406),
-                std=(0.229, 0.224, 0.225),
-            ),
+            #transforms.Normalize(
+            #    mean=(0.485, 0.456, 0.406),
+            #    std=(0.229, 0.224, 0.225),
+            #),
         ]
     )
 
@@ -56,10 +56,10 @@ def make_val_transform(img_size: int = 224):
             transforms.Resize(256),
             transforms.CenterCrop(img_size),
             transforms.ToTensor(),
-            transforms.Normalize(
-                mean=(0.485, 0.456, 0.406),
-                std=(0.229, 0.224, 0.225),
-            ),
+            #transforms.Normalize(
+            #    mean=(0.485, 0.456, 0.406),
+            #    std=(0.229, 0.224, 0.225),
+            #),
         ]
     )
 
